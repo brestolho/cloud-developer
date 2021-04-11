c16e7695d27411f5a7bb6f53ae7eb1dac0bca24c
Bruno <>
2021-04-11 02:16:53 +0100

    Add filteredimage endpoint to server

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -1,46 +1,56 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import {filterImageFromURL, deleteLocalFiles} from './util/util';
+import express from "express";
+import bodyParser from "body-parser";
+import { filterImageFromURL, deleteLocalFiles, getFileNameFromPath } from "./util/util";
 
 (async () => {
-
   // Init the Express application
   const app = express();
 
   // Set the network port
   const port = process.env.PORT || 8082;
-  
+
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
-  // GET /filteredimage?image_url={{URL}}
-  // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
-  // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
+  // Creaye middleware for deleting files cached after success response
+  app.use((req, res, next) => {
+    res.on("finish", () => {
+      // get headers
+      const headers: any = res.getHeaders();
+      // get filename to delete
+      const udacityFilename: string = headers["udacity-filename"];
+      if (udacityFilename) {
+        // delete file with path prefix
+        deleteLocalFiles([`${__dirname}/util/tmp/${udacityFilename}`]);
+      }
+    });
+    next();
+  });
 
-  /**************************************************************************** */
-
-  //! END @TODO1
-  
   // Root Endpoint
   // Displays a simple message to the user
-  app.get( "/", async ( req, res ) => {
-    res.send("try GET /filteredimage?image_url={{}}")
-  } );
-  
+  app.get("/", async (req, res) => {
+    res.send("try GET /filteredimage?image_url={{}}");
+  });
+
+  // Creaye endpoint filteredimage, cache image file from a query source
+  app.get("/filteredimage", async (req, res) => {
+    const { image_url }: { image_url: string } = req.query;
+    // validate if query param is not empty
+    if (!image_url) {
+      return res.status(400).send({ message: "Image url is required!" });
+    }
+
+    // filter image and store it locally
+    let imagePath: string = await filterImageFromURL(image_url);
+
+    // response stored image to user as a proxy
+    res.status(200).header("udacity-filename", getFileNameFromPath(imagePath)).sendFile(imagePath);
+  });
 
   // Start the Server
-  app.listen( port, () => {
-      console.log( `server running http://localhost:${ port }` );
-      console.log( `press CTRL+C to stop server` );
-  } );
+  app.listen(port, () => {
+    console.log(`server running http://localhost:${port}`);
+    console.log(`press CTRL+C to stop server`);
+  });
 })();
